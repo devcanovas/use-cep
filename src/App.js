725e7307b3f96e37fa3171c2a9367ccc6815e8a0
Matheus Canovas <matheuscanovas9@gmail.com>
2024-01-05 12:43:53 -0300

    Upando o projeto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [address, setAddress] = useState('');
+
+  const requestAddress = (e) => {
+    const postalcode = e.target.value
+    setAddress({
+      postalcode: postalcode
+    })
+    if (postalcode && postalcode.length === 8) {
+      fetch(`https://viacep.com.br/ws/${postalcode}/json/`,)
+        .then(response => response.json())
+        .then(data => {
+          setAddress({
+            postalcode: postalcode,
+            street: data.logradouro,
+            neighborhood: data.bairro,
+            province: data.localidade,
+            ibge: data.ibge
+          })
+        })
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          placeholder='Digite seu CEP'
+          type='text'
+          onChange={requestAddress}
+        />
+        <ul>
+          <li>CEP: {address.postalcode}</li>
+          <li>Logradouro: {address.street}</li>
+          <li>Bairro: {address.neighborhood}</li>
+          <li>Código IBGE: {address.ibge}</li>
+        </ul>
       </header>
     </div>
   );
